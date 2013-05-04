df7e4f3708a0ea3c5587f56417d525064e68a375
oreissig <oreissig@gmail.com>
2013-05-04 15:06:01 +0200

    fix commit

--- compiler/src/fortran/lexer/StatementHandler.java
+++ compiler/src/fortran/lexer/StatementHandler.java
@@ -1,34 +1,79 @@
 package fortran.lexer;
 
 import java.util.Iterator;
-import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.AbstractIterator;
+import com.google.common.collect.Iterators;
+import com.google.common.collect.PeekingIterator;
 
 import fortran.reader.Card;
 
-class LexerImpl extends StatementHandler {
+class LexerImpl extends AbstractIterator<Statement> implements Lexer {
 
-	private Card current;
+	private static final Logger log = LoggerFactory.getLogger(Lexer.class);
+
+	private final PeekingIterator<Card> cards;
+	private Card card = null;
 	private int position = -1;
 
-	public LexerImpl(Iterable<Card> cards) {
-		super(cards);
+	/**
+	 * Creates a new Lexer based on the given stream of {@link Card}s.
+	 * 
+	 * @param cards
+	 *            to lex
+	 */
+	public LexerImpl(Iterator<Card> cards) {
+		if (cards instanceof PeekingIterator)
+			this.cards = (PeekingIterator<Card>) cards;
+		else
+			this.cards = Iterators.peekingIterator(cards);
 	}
 
-	public LexerImpl(Iterator<Card> cards) {
-		super(cards);
+	/**
+	 * Creates a new Lexer based on the given {@link Card}s.
+	 * 
+	 * @param cards
+	 *            to lex
+	 */
+	public LexerImpl(Iterable<Card> cards) {
+		this(cards.iterator());
 	}
 
 	@Override
-	protected List<Literal> lex(Card card) {
+	protected Statement computeNext() {
+		final Statement stmt = new StatementImpl();
+
+		if (card == null) {
+			if (!nextCard())
+				endOfData();
+		}
+
+		// TODO
+		return endOfData();
+	}
+
+	/**
+	 * Reads the next non-comment card.
+	 * 
+	 * @return true on success, false if no more cards
+	 */
+	private boolean nextCard() {
 		position = 0;
-		current = card;
-		
-		// TODO Auto-generated method stub
-		return null;
+		do {
+			if (cards.hasNext()) {
+				card = cards.next();
+			} else {
+				return false;
+			}
+		} while (card.isComment());
+		return true;
 	}
 
 	private boolean expect(String toBeExpected) {
-		String stmt = current.getStatement();
+		String stmt = card.getStatement();
 		// enough chars left?
 		if ((stmt.length() - position) < toBeExpected.length())
 			return false;
