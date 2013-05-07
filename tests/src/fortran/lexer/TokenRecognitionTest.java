2f9f502c0ed62de36a89248ca6b6c7d5345bdfbe
oreissig <oreissig@gmail.com>
2013-05-07 19:32:49 +0200

    extend Lexer tests

--- tests/src/fortran/lexer/TokenRecognitionTest.java
+++ tests/src/fortran/lexer/TokenRecognitionTest.java
@@ -6,9 +6,12 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.junit.Test;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.UnmodifiableIterator;
 
 import fortran.reader.Card;
@@ -17,6 +20,7 @@ import fortran.reader.TestCard;
 public class TokenRecognitionTest {
 	@Test
 	public void testKeywords() {
+		// manual page 55
 		Lexer l = create(Keywords.names);
 		for (TokenType type : Keywords.set) {
 			Statement stmt = l.next();
@@ -29,7 +33,10 @@ public class TokenRecognitionTest {
 	@Test
 	public void testIntConstants() {
 		// manual page 9
-		String[] inputs = {"3", "+1", "-28987"};
+		String[] inputs = {
+				"3",
+				"+1",
+				"-28987" };
 		Lexer l = create(inputs);
 		for (String s : inputs) {
 			List<Token> tokens = l.next().getTokens();
@@ -43,13 +50,19 @@ public class TokenRecognitionTest {
 	@Test
 	public void testFloatConstants() {
 		// manual page 9
-		String[] inputs = {"17.", "5.0", "-.0003", "5.0E3", "5.0E+3", "5.0E-7"};
+		String[] inputs = {
+				"17.",
+				"5.0",
+				"-.0003",
+				"5.0E3",
+				"5.0E+3",
+				"5.0E-7" };
 		Lexer l = create(inputs);
 		for (String s : inputs) {
 			List<Token> tokens = l.next().getTokens();
 			assertEquals(1, tokens.size());
 			Token t = tokens.get(0);
-			assertEquals(CONST_INT, t.getType());
+			assertEquals(CONST_FLOAT, t.getType());
 			assertEquals(s, t.getText());
 		}
 	}
@@ -57,7 +70,10 @@ public class TokenRecognitionTest {
 	@Test
 	public void testIntVariables() {
 		// manual page 10
-		String[] inputs = {"I", "M2", "JOBN0"};
+		String[] inputs = {
+				"I",
+				"M2",
+				"JOBN0" };
 		Lexer l = create(inputs);
 		for (String s : inputs) {
 			List<Token> tokens = l.next().getTokens();
@@ -71,7 +87,10 @@ public class TokenRecognitionTest {
 	@Test
 	public void testFloatVariables() {
 		// manual page 10
-		String[] inputs = {"A", "B7", "DELTA"};
+		String[] inputs = {
+				"A",
+				"B7",
+				"DELTA" };
 		Lexer l = create(inputs);
 		for (String s : inputs) {
 			List<Token> tokens = l.next().getTokens();
@@ -84,8 +103,13 @@ public class TokenRecognitionTest {
 
 	@Test
 	public void testIntFunctions() {
-		// manual page 12
-		String[] inputs = {"SINF", "SOMEF", "SQRTF", "SIN0F", "SIN1F"};
+		// manual page 12-13
+		String[] inputs = {
+				"SINF",
+				"SOMEF",
+				"SQRTF",
+				"SIN0F",
+				"SIN1F" };
 		Lexer l = create(inputs);
 		for (String s : inputs) {
 			List<Token> tokens = l.next().getTokens();
@@ -96,8 +120,13 @@ public class TokenRecognitionTest {
 		}
 	}
 
+	@Test
 	public void testFloatFunctions() {
-		String[] inputs = {"XTANF", "XSIN0F", "XSIN1F"};
+		// manual page 12-13
+		String[] inputs = {
+				"XTANF",
+				"XSIN0F",
+				"XSIN1F" };
 		Lexer l = create(inputs);
 		for (String s : inputs) {
 			List<Token> tokens = l.next().getTokens();
@@ -109,9 +138,28 @@ public class TokenRecognitionTest {
 	}
 
 	@Test
-	public void testMisc() {
-		// TODO test misc. tokens
-		fail();
+	public void testOpsAndMisc() {
+		// manual page 14-15
+		Map<String, TokenType> inputs = ImmutableMap.<String, TokenType> builder()
+				.put("(", PAREN1)
+				.put(")", PAREN2)
+				.put(",", COMMA)
+				.put("=", EQUALS)
+				.put("+", PLUS)
+				.put("-", MINUS)
+				.put("*", MUL)
+				.put("**", EXP)
+				.put("/", DIV)
+				.build();
+
+		Lexer l = create(inputs.keySet());
+		for (Entry<String, TokenType> e : inputs.entrySet()) {
+			List<Token> tokens = l.next().getTokens();
+			assertEquals(1, tokens.size());
+			Token t = tokens.get(0);
+			assertEquals(e.getValue(), t.getType());
+			assertEquals(e.getKey(), t.getText());
+		}
 	}
 
 	private Lexer create(final Iterable<String> input) {
@@ -130,7 +178,6 @@ public class TokenRecognitionTest {
 		};
 		return new LexerImpl(cards);
 	}
-	
 
 	private Lexer create(final String[] input) {
 		return create(Arrays.asList(input));
