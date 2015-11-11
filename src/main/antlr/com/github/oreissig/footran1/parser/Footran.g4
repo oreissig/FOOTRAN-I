f910a4acd59eba9550c4828c265c5e2c67d8f9f9
oreissig <oreissig@gmail.com>
2015-11-11 23:47:57 +0100

    refine lexer grammar

--- src/main/antlr/com/github/oreissig/footran1/parser/Footran.g4
+++ src/main/antlr/com/github/oreissig/footran1/parser/Footran.g4
@@ -21,13 +21,13 @@ fpConst  : ('+'|'-')? NUMBER? '.' NUMBER? ('E' intConst)? ;
 
 // prefix area processing
 COMMENT : {getCharPositionInLine() == 0}? 'C' ~('\n')* '\n'? -> skip ;
-STMTNUM : {getCharPositionInLine() < 6}? [0-9]+ ;
-CONTINUE : '\n' . . . . . ~[' '|'0'] -> skip ;
+STMTNUM : {getCharPositionInLine() < 5}? [0-9]+ ;
+CONTINUE : NEWCARD . . . . . ~[' '|'0'] -> skip ;
 
 // body processing
-ID      : {getCharPositionInLine() > 6}? [A-Z][A-Z0-9]+ ;
-NUMBER  : {getCharPositionInLine() > 6}? [0-9]+ ;
+ID      : {getCharPositionInLine() > 5}? [A-Z][A-Z0-9]* ;
+NUMBER  : {getCharPositionInLine() > 5}? [0-9]+ ;
 // return newlines to parser
 NEWCARD : '\r'? '\n' ;
 // skip spaces and tabs
-WS      : [ \t]+ -> skip ;
+WS      : ' '+ -> skip ;
--- src/test/groovy/com/github/oreissig/footran1/parser/LexerSpec.groovy
+++ src/test/groovy/com/github/oreissig/footran1/parser/LexerSpec.groovy
@@ -1,9 +1,11 @@
 package com.github.oreissig.footran1.parser
 
 import static com.github.oreissig.footran1.parser.FootranLexer.*
+import spock.lang.Stepwise
 import spock.lang.Unroll
 
 @Unroll
+@Stepwise
 public class LexerSpec extends AbstractFootranSpec {
     
     def 'empty program lexes successfully'() {
@@ -36,5 +38,44 @@ C1234XBARBAZ
         src << ['    1 ',
                 ' 1337 ',
                 '32767 ',]
+    }
+    
+    def 'numbers are recognized (#num)'(num) {
+        given:
+        input = "      $num"
+        
+        expect:
+        tokens.size() == 1
+        def t = tokens.first()
+        t.type == NUMBER
+        t.text == num
+        
+        where:
+        num << ['1', '23', '32767']
+    }
+    
+    def 'identifiers are recognized (#id)'(id) {
+        given:
+        input = "      $id"
+        
+        expect:
+        tokens.size() == 1
+        def t = tokens.first()
+        t.type == ID
+        t.text == id
+        
+        where:
+        id << ['A', 'I', 'B7', 'SINF', 'XTANF']
+    }
+    
+    def 'continuations work'() {
+        given:
+        input = '''\
+      123
+     XABC'''
+        
+        expect:
+        tokens*.type == [NUMBER, ID]
+        tokens*.text == ['123', 'ABC']
     }
 }
